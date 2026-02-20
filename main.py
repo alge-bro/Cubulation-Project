585f74f38d381741ed20ec23514f08377ec68a06
Caden <cadenhol@buffalo.edu>
2026-02-20 10:25:50 -0500

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from simplex_program.simplex_algorithm import simplexAlgorithm
-
-testSet = frozenset([frozenset({1,2,3}),frozenset({1,2,4}),frozenset({2,3,4})])
-size = 4
-testObj = simplexAlgorithm(testSet,size)
-testObj.checkIfValidComplex()
-testObj.checkIfComplexIsFlag()
